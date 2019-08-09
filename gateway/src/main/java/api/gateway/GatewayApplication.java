983475209d22a7c2bb4499e9888271fd071b8196
13625025773 <975950982@qq.com>
2019-08-09 15:37:09 +0800

    面向服务的路由
    面向服务的路由

--- gateway/src/main/java/api/gateway/GatewayApplication.java
+++ gateway/src/main/java/api/gateway/GatewayApplication.java
@@ -1,13 +1,15 @@
 package api.gateway;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        new SpringApplicationBuilder(GatewayApplication.class).web(true).run(args);
     }
 
 }
