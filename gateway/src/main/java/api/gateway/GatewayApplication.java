9c345c23993b6d01a7e06684075e3e20109c1472
13625025773 <975950982@qq.com>
2019-08-09 15:47:53 +0800

    请求过滤
    请求过滤

--- gateway/src/main/java/api/gateway/GatewayApplication.java
+++ gateway/src/main/java/api/gateway/GatewayApplication.java
@@ -1,8 +1,10 @@
 package api.gateway;
 
+import api.gateway.filter.AccessFilter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(GatewayApplication.class).web(true).run(args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
     }
 
 }
